5b02dff01b775537466aa341daafddd4e248f69e
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2019-04-24 20:58:07 -0400

    Fix bug generating TOPO file without Greenland.

--- topo/regenerate_focean1_gmask.py
+++ topo/regenerate_focean1_gmask.py
@@ -12,8 +12,8 @@ ofname = 'focean1m_gmask.png'
 oncname = 'ZETOPO1.NCEI-SeparateGreenland.nc'
 
 # Read continents from ETOPO1
-with netCDF4.Dataset(incname) as nc:
-    ncvar = nc.variables[ivname]
+with netCDF4.Dataset(incname) as ncin:
+    ncvar = ncin.variables[ivname]
     focean1m = np.zeros(ncvar.shape, dtype=np.uint8)
     focean1m[:] = ncvar[:]
 
@@ -31,7 +31,8 @@ gindices = np.cumsum(indices1)
 
 # Mask them out!
 focean1m1 = focean1m.reshape(-1)
-focean1m1[gindices] = 2
+gindices_land = gindices[focean1m1[gindices] == 0]   # Only mask out land areas
+focean1m1[gindices_land] = 2
 
 # ------------------------------------------------------
 # Generate the NC file
@@ -41,7 +42,10 @@ with netCDF4.Dataset(incname, 'r') as ncin:
 
         first = True
         for vname in ('FOCEAN', 'ZICTOP', 'ZSOLID'):
-            val1m_gridreg = ncin.variables[vname][:]
+            if vname == 'FOCEAN':
+                val1m_gridreg = focean1m
+            else:
+                val1m_gridreg = ncin.variables[vname][:]
 
             # Convert grid-registered to pixel-registerd
             # We can fudge that by just dropping the last point
@@ -56,13 +60,21 @@ with netCDF4.Dataset(incname, 'r') as ncin:
                 im1m_d = ncout.createDimension('im1m', size=val1m.shape[1])
 
             # Create variable
+            print('creating {}'.format(vname))
             ncvar = ncout.createVariable(vname, 'd', ('jm1m', 'im1m'))
             ncvar[:] = val1m[:]
 
             first = False
 
 # ------------------------------------------------------
-# Generate the PNG file
+# Generate the PNG file (from the NetCDF file)
+
+focean1m = None
+with netCDF4.Dataset(oncname) as ncin:
+    vname = 'FOCEAN'
+    ncvar = ncin.variables[ivname]
+    focean1m = np.zeros(ncvar.shape, dtype=np.uint8)
+    focean1m[:] = ncvar[:]
 
 print('Saving the PNG file')
 
@@ -76,9 +88,9 @@ for i in range(0,256):
     palette[i,:] = 255-i
 
 # Replace certain colors
-palette[0,:] = (0,255,0)        # FOCEAN=0 = Land
-palette[1,:] = (0,0,255)        # 1 = Ocean
-palette[2,:] = (255,255,255)    # 2 = Greenland
+palette[0,:] = (0,255,0)        # FOCEAN=0 = Land: Green
+palette[1,:] = (0,0,255)        # 1 = Ocean: Blue
+palette[2,:] = (255,255,255)    # 2 = Greenland: White
 
 pl = list(palette.reshape(-1))
 img.putpalette(pl)
