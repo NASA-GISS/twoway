7e3806184dc92b0ea54dda109b83b6b87e715c1b
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2019-09-11 20:04:10 -0400

    Almost works, but something is wrong with Greenland grid

--- topo/modele_pism_inputs.py
+++ topo/modele_pism_inputs.py
@@ -5,6 +5,10 @@
 import argparse
 import subprocess
 import contextlib, os
+import re
+import netCDF4
+import collections
+import numpy as np
 
 @contextlib.contextmanager
 def pushd(dir):
@@ -36,15 +40,27 @@ makefile_str = """
 all : topoa.nc
 
 # ----------------- Things we make here
+{gridA} {global_ecO_ng} {topoo_ng} :
+	cd {topo_root}; $(MAKE) {gridA_leaf} {global_ecO_ng_leaf} {topoo_ng_leaf}
 
-{exgrid}: {gridA} {gridI}
+
+{gridI} : # only re-generate if it doesn't exist, it is named by content
+	spec_to_grid gridI_spec.nc -o {gridI}   # gridI_spec.nc was generated from PISM state file
+
+{exgrid} : {gridI} {gridA}
 	overlap {gridA} {gridI} -o {exgrid}
+
+# Just for kicks
+gridI.nc : {gridI}
+	ln -s {gridI} gridI.nc
+exgridO.nc : {exgrid}
+	ln -s {exgrid} exgridO.nc
+
 
 gcmO.nc : {exgrid} {pism_state} {gridA} {gridI}
 	echo '*****************************************************************'
 	echo '[makefile] Assembling IceBin Input File from grids (contains loadable gcmO).'
 	python3 {topo_root}/write_icebin_in_base.py {gridA} {gridI} {exgrid} {pism_state} ./gcmO.nc
-
 
 topoo_merged.nc : {pism_state} gcmO.nc {global_ecO_ng} {topoo_ng}
         # Merge without squashing
@@ -64,9 +80,194 @@ topoa.nc : topoa_nc4.nc
 """
 
 
+# IceBin-specific rundeck extension...
+icebin_cdl_str = """
+// Sample Icebin parameter file.  Copy to [rundir]/config/icebin.cdl
+//
+// See also the following rundeck paramters to control the Stieglitz snow/firn model:
+// (LISnowParams.F90`` for documentation)
+//      lisnow_target_depth
+//      lisnow_rho_fresh_snow
+//      lisnow_rho_snow_firn_cutoff
+//      lisnow_max_fract_water
+//      lisnow_epsilon
+//      lisnow_min_snow_thickness
+//      lisnow_min_fract_cover
+//      lisnow_dump_forcing
+//      lisnow_percolate_nl
+
+netcdf icebin {
+variables:
+    // Setup methods (in ectl) to be run after symlinks are
+    // created and before ModelE is launched.
+    // Names of these attributes can be whatever you like
+    int setups ;
+        setups:landice = "ectl.xsetup.pism_landice.xsetup";
+
+    // ModelE-specific variables cover all ice sheets
+    int m.info ;
+
+        m.info:grid = "input-file:./input/gcmO.nc";
+
+        // Where IceBin may write stuff
+        m.info:output_dir = "output-dir:icebin";
+
+        // Normally, set use_smb="t"
+        // FOR TESTING ONLY: set to "f" and IceBin will pass a zero SMB and
+        // appropriately zero B.C. to the ice sheet.
+        m.info:use_smb = "t" ;
+
+    // Additional variables agument m.greenland.info from main icebin_in
+    // These variables are specific to Greenland
+    int m.greenland.info ;
+        // Output for greenland-specific stuff
+        m.greenland.info:output_dir = "output-dir:greenland";
+
+        // The ice model with which we are coupling.
+        // See IceCoupler::Type [DISMAL, PISM, ISSM, WRITER]
+        m.greenland.info:ice_coupler = "PISM" ;
+
+        // Should we upate the elevation field in update_ice_sheet()?
+        // Normally, yes.  But in some TEST CASES ONLY --- when the SMB
+        // field was created with a different set of elevations than the
+        // ice model is using --- then this can cause problems in the
+        // generated SMB fields.
+        // See IceModel_PISM::update_elevation
+        m.greenland.info:update_elevation = "t" ;
+
+        // Variable currently in icebin_in, but maybe they should be moved here.
+        // m.greenland.info:interp_grid = "EXCH" ;
+        // m.greenland.info:interp_style = "Z_INTERP" ;
+        // Also: hcdefs, indexingHC
+
+    // Variables specific to the ModelE side of the coupling
+    double m.greenland.modele ;
+
+        // Should ModelE prepare for Dirichlet or Neumann boundary
+        // conditions with the dynamic ice model?
+        // See 
+        m.greenland.modele:coupling_type = "DIRICHLET_BC" ;
+
+    double m.greenland.pism ;
+        // Command-line arguments provided to PISM upon initialization
+        // Paths will be resolved for filenames in this list.
+        m.greenland.pism:i = "input-file:pism/std-greenland/g20km_10ka.nc" ;
+        m.greenland.pism:skip = "" ;
+        m.greenland.pism:skip_max = "10" ;
+        m.greenland.pism:surface = "given" ;
+        m.greenland.pism:surface_given_file = "input-file:pism/std-greenland/pism_Greenland_5km_v1.1.nc" ;
+        m.greenland.pism:calving = "ocean_kill" ;
+        m.greenland.pism:ocean_kill_file = "input-file:pism/std-greenland/pism_Greenland_5km_v1.1.nc" ;
+        m.greenland.pism:sia_e = "3.0" ;
+        m.greenland.pism:ts_file = "output-file:greenland/ts_g20km_10ka_run2.nc" ;
+        m.greenland.pism:ts_times = "0:1:1000" ;
+        m.greenland.pism:extra_file = "output-file:greenland/ex_g20km_10ka_run2.nc" ;
+        m.greenland.pism:extra_times = "0:.1:1000" ;
+        m.greenland.pism:extra_vars = "climatic_mass_balance,ice_surface_temp,diffusivity,temppabase,tempicethk_basal,bmelt,tillwat,csurf,mask,thk,topg,usurf" ;
+        m.greenland.pism:o = "g20km_10ka_run2.nc" ;
+    double m.greenland.dismal ;
+        m.greenland.dismal:output_dir = "dismal_out2" ;
+
+}
+"""
+
+def snoop_pism(pism_state):
+    """Snoops around a PISM run directory, looking for key files and
+    strings needed to set up IceBin.
+    pism_state:
+        Name of the PISM state file
+    """
+    # Directory of the PISM run
+    pism_state = os.path.realpath(pism_state)
+    pism_dir = os.path.split(pism_state)[0]
+
+    # Read the main PISM state file
+    vals = collections.OrderedDict()
+    with netCDF4.Dataset(pism_state) as nc:
+        # Read command line
+        cmd = re.split(r'\s+', nc.command)
+        config_nc = nc.variables['pism_config']
+        Mx = int(getattr(config_nc, 'grid.Mx'))
+        My = int(getattr(config_nc, 'grid.My'))
+        vals['grid.Mx'] = Mx
+        vals['grid.My'] = My
+
+    # Parse command line into (name, value pairs)
+    args = collections.OrderedDict()
+    vals['args'] = args
+    for i in range(0,len(cmd)):
+        if cmd[i].startswith('-'):
+            args[cmd[i][1:]] = cmd[i+1]
+            i += 1
+
+    # Get absolute name of files given in PISM command line as relative
+    for key in ('i', 'surface_given_file', 'ocean_kill_file', 'ts_file', 'extra_file', 'o'):
+        args[key] = os.path.normpath(os.path.join(pism_dir, args[key]))
+
+    # Read the 'i' file for more info
+    with netCDF4.Dataset(args['i']) as nc:
+        vals['proj4'] = nc.proj4
+        xc5 = nc.variables['x1'][:]    # Cell centers (for standard 5km grid)
+        yc5 = nc.variables['y1'][:]
+
+    # Determine cell centers on our chosen resolution
+    xc = np.array(list(xc5[0] + (xc5[-1]-xc5[0]) * ix / (Mx-1) for ix in range(0,Mx)))
+    yc = np.array(list(yc5[0] + (yc5[-1]-yc5[0]) * iy / (My-1) for iy in range(0,My)))
+    vals['x_centers'] = xc
+    vals['y_centers'] = yc
+    #vals['index_order'] = (0,1)    # Later version does SeaRise order
+    vals['index_order'] = (1,0)    # Later version does SeaRise order
+
+    # Name grid after name of input file
+    idx = int(.5 + (xc[1] - xc[0]) / 1000.)
+    idy = int(.5 + (yc[1] - yc[0]) / 1000.)
+    if idx == idy:
+        dxdy = '{}'.format(idx)
+    else:
+        dxdy = '{}_{}'.format(idx,idy)
+
+    iname = os.path.split(args['i'])[1]
+    if iname.startswith('pism_Greenland'):
+        vals['name'] = 'pism_g{}km_{}{}'.format(dxdy, vals['index_order'][0], vals['index_order'][1])
+    else:
+        vals['name'] = '{}{}km_{}'.format(os.path.splitext(iname)[0], dxdy, vals['index_order'][0], vals['index_order'][1])
+
+    return vals
+
+def center_to_boundaries(xc):
+    xb = np.zeros(len(xc)+1)
+    xb[0] = 1.5*xc[0]-.5*xc[1]
+    xb[1:-1] = (xc[0:-1] + xc[1:]) * .5
+    xb[-1] = 1.5*xc[-1]-.5*xc[-2]
+    return xb
+
+def write_gridspec_xy(pism, spec_fname):
+    """Given info gleaned from PISM, writes it out as a GridSpec_XY that
+    can be read by the grid generator."""
+
+    with netCDF4.Dataset(spec_fname, 'w') as nc:
+        xc = pism['x_centers']
+        xb = center_to_boundaries(xc)
+        nc.createDimension('grid.x_boundaries.length', len(xb))
+        nc_xb = nc.createVariable('grid.x_boundaries', 'd', ('grid.x_boundaries.length'))
+        nc_xb[:] = xb
+
+        yc = pism['y_centers']
+        yb = center_to_boundaries(yc)
+        nc.createDimension('grid.y_boundaries.length', len(yb))
+        nc_yb = nc.createVariable('grid.y_boundaries', 'd', ('grid.y_boundaries.length'))
+        nc_yb[:] = yb
+
+        nc_info = nc.createVariable('grid.info', 'i', ())
+        nc_info.setncattr('name', pism['name'])
+        nc_info.type = 'XY'
+        nc_info.indices = (0,1)    # Old PISM indexing order; reverse for new PISM / SeaRISE
+        nc_info.sproj = pism['proj4']
+        nc_info.nx = len(xc)
+        nc_info.ny = len(yc)
+
 
 def modele_pism_inputs(topo_root, coupled_dir, pism_state,
-    gridI_cmd, gridI_leaf,
     grid_dir=None):
 
     """twoway_root:
@@ -86,23 +287,29 @@ def modele_pism_inputs(topo_root, coupled_dir, pism_state,
     if grid_dir is None:
         grid_dir = topo_root
     os.makedirs(grid_dir, exist_ok=True)
+    os.makedirs(coupled_dir, exist_ok=True)
 
     # Create ModelE grid and other general stuff (not PISM-specific)
     gridA_leaf = 'modele_ll_g1qx1.nc'
-    with pushd(topo_root):
-        cmd = ['make', gridA_leaf, 'global_ecO_ng.nc', 'topoo_ng.nc']
     repl = dict(    # Used to create templated makefile
-        pism_state=pism_state,
-        topo_root=topo_root,
-        gridA=os.path.join(topo_root, gridA_leaf),
-        global_ecO_ng=os.path.join(topo_root, 'global_ecO_ng.nc'),
-        topoo_ng=os.path.join(topo_root, 'topoo_ng.nc'))
+        pism_state = pism_state,
+        topo_root = topo_root,
+
+        gridA = os.path.join(topo_root, gridA_leaf),
+        global_ecO_ng = os.path.join(topo_root, 'global_ecO_ng.nc'),
+        topoo_ng = os.path.join(topo_root, 'topoo_ng.nc'),
+
+        gridA_leaf = gridA_leaf,
+        global_ecO_ng_leaf = 'global_ecO_ng.nc',
+        topoo_ng_leaf = 'topoo_ng.nc')
 
 
-    # Create the PISM grid
+    # Create the PISM grid spec
+    pism = snoop_pism(pism_state)
+    write_gridspec_xy(pism, os.path.join(coupled_dir, 'gridI_spec.nc'))
+    gridI_leaf = '{}.nc'.format(pism['name'])
     gridI_fname = os.path.join(grid_dir, gridI_leaf)
     repl['gridI'] = gridI_fname
-    make_grid(gridI_cmd, gridI_fname)
 
     # Overlap the two
     exgrid_leaf = '{}-{}'.format(os.path.splitext(gridA_leaf)[0], gridI_leaf)
@@ -110,11 +317,13 @@ def modele_pism_inputs(topo_root, coupled_dir, pism_state,
 
     # Do the rest
     print('************** Makefile')
-    os.makedirs(coupled_dir, exist_ok=True)
     with pushd(coupled_dir):
-        with open('Makefile', 'w') as fout:
+        with open('icebin.cdl', 'w') as fout:
+            fout.write(icebin_cdl_str)
+
+        with open('modele_pism_inputs.mk', 'w') as fout:
             fout.write(makefile_str.format(**repl))
-        cmd = ['make', 'topoa.nc', 'topo_oc.nc']
+        cmd = ['make', '-f', 'modele_pism_inputs.mk', 'topoa.nc', 'topo_oc.nc']
         print(' '.join(cmd))
         subprocess.run(cmd)
 
@@ -129,16 +338,17 @@ def main():
     parser.add_argument('--out', dest='coupled_dir',
         required=True,
         help="Name of directory for output files.  Eg: <ectl-run>/inputs")
-    parser.add_argument('--pism_grid_cmd', dest='pism_grid_cmd',
-        default='searise_grid --index_order searise',
-        help="Command line (space-separated) required to create PISM grid.")
-    parser.add_argument('--pism_grid_leaf', dest='pism_grid_leaf',
-        default='sr_g20_searise.nc',
-        help="Output file of PISM grid (just leafname)")
     args = parser.parse_args()
-    print(args.coupled_dir)
+
+    pism = snoop_pism(args.pism)
+    write_gridspec_xy(pism, 'x.nc')
+
+    for k,v in pism['args'].items():
+        if k != 'args':
+            print(k,v)
 
     modele_pism_inputs(topo_root, os.path.realpath(args.coupled_dir), os.path.realpath(args.pism),
-        args.pism_grid_cmd.split(' '), args.pism_grid_leaf, grid_dir=topo_root)
+        grid_dir=topo_root)
 
 main()
+
